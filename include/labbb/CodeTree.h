b014ba71ee586047e48c5c1274474f8d706482ab
omerfaruk <omer.erzurumluoglu@std.bogazici.edu.tr>
2024-09-11 05:34:20 +0300

    Before implementing visitor interface for LTLNode

--- include/labbb/CodeTree.h
+++ include/labbb/CodeTree.h
@@ -43,7 +43,62 @@ public:
     static void* operator new(size_t size);
     virtual std::variant<DdNode*, bool>  operate();
     virtual size_t hash();
-    virtual bool operator==(const LTLNode& other);
+    virtual bool operator==(const LTLNode& other) {
+        if (op != other.op) return false;
+        switch (op)
+        {
+        case NOT:
+            const NOTNode* otherNotNode = dynamic_cast<const NOTNode*>(&other);
+            const NOTNode* thisNotNode = dynamic_cast<const NOTNode*>(this);
+            return thisNotNode == otherNotNode;
+        case AND:
+            const ANDNode* otherAndNode = dynamic_cast<const ANDNode*>(&other);
+            const ANDNode* thisAndNode = dynamic_cast<const ANDNode*>(this);
+            return thisAndNode == otherAndNode;
+        case OR:
+            const ORNode* otherOrNode = dynamic_cast<const ORNode*>(&other);
+            const ORNode* thisOrNode = dynamic_cast<const ORNode*>(this);
+            return thisOrNode == otherOrNode;
+        case SINCE:
+            const SINCENode* otherSinceNode = dynamic_cast<const SINCENode*>(&other);
+            const SINCENode* thisSinceNode = dynamic_cast<const SINCENode*>(this);
+            return thisSinceNode == otherSinceNode;
+        case PRE:
+            const PRENode* otherPreNode = dynamic_cast<const PRENode*>(&other);
+            const PRENode* thisPreNode = dynamic_cast<const PRENode*>(this);
+            return thisPreNode == otherPreNode;
+        case ONCE:
+            const ONCENode* otherOnceNode = dynamic_cast<const ONCENode*>(&other);
+            const ONCENode* thisOnceNode = dynamic_cast<const ONCENode*>(this);
+            return thisOnceNode == otherOnceNode;
+        case HISTORICALLY:
+            const HISTORICALLYNode* otherHistoricallyNode = dynamic_cast<const HISTORICALLYNode*>(&other);
+            const HISTORICALLYNode* thisHistoricallyNode = dynamic_cast<const HISTORICALLYNode*>(this);
+            return thisHistoricallyNode == otherHistoricallyNode;
+        case EXISTS:
+            const EXISTSNode* otherExistsNode = dynamic_cast<const EXISTSNode*>(&other);
+            const EXISTSNode* thisExistsNode = dynamic_cast<const EXISTSNode*>(this);
+            return thisExistsNode == otherExistsNode;
+        case FORALL:
+            const FORALLNode* otherForallNode = dynamic_cast<const FORALLNode*>(&other);
+            const FORALLNode* thisForallNode = dynamic_cast<const FORALLNode*>(this);
+            return thisForallNode == otherForallNode;
+        case IMPLIES:
+            const IMPLIESNode* otherImpliesNode = dynamic_cast<const IMPLIESNode*>(&other);
+            const IMPLIESNode* thisImpliesNode = dynamic_cast<const IMPLIESNode*>(this);
+            return thisImpliesNode == otherImpliesNode;
+        case ATOM:
+            const ATOMNode* otherAtomNode = dynamic_cast<const ATOMNode*>(&other);
+            const ATOMNode* thisAtomNode = dynamic_cast<const ATOMNode*>(this);
+            return thisAtomNode == otherAtomNode;
+        case ATOM_REF:
+            const ATOM_REFNode* otherAtomRefNode = dynamic_cast<const ATOM_REFNode*>(&other);
+            const ATOM_REFNode* thisAtomRefNode = dynamic_cast<const ATOM_REFNode*>(this);
+            return thisAtomRefNode == otherAtomRefNode;
+        default:
+            return false;
+        }
+    }
 };
 
 class NOTNode : public LTLNode {
@@ -54,9 +109,8 @@ public:
     size_t hash() {
         return ::hash( {op, child->id} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return child->id == ((NOTNode*)&other)->child->id;
+    bool operator==(const NOTNode& other) {
+        return child->id == other.child->id;
     }
 };
 
@@ -71,10 +125,9 @@ public:
         int big = std::max(left->id, right->id);
         return ::hash( {op, small, big} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return (left->id == ((ANDNode*)&other)->left->id && right->id == ((ANDNode*)&other)->right->id) ||
-               (left->id == ((ANDNode*)&other)->right->id && right->id == ((ANDNode*)&other)->left->id);
+    bool operator==(const ANDNode& other) {
+        return (left->id == other.left->id && right->id == other.right->id) ||
+               (left->id == other.right->id && right->id == other.left->id);
     }
 };
 
@@ -89,10 +142,9 @@ public:
         int big = std::max(left->id, right->id);
         return ::hash( {op, small, big} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return (left->id == ((ORNode*)&other)->left->id && right->id == ((ORNode*)&other)->right->id) ||
-               (left->id == ((ORNode*)&other)->right->id && right->id == ((ORNode*)&other)->left->id);
+    bool operator==(const ORNode& other) {
+        return (left->id == other.left->id && right->id == other.right->id) ||
+               (left->id == other.right->id && right->id == other.left->id);
     }
 };
 
@@ -106,10 +158,9 @@ public:
     size_t hash() {
         return ::hash( {op, time.from, time.to, left->id, right->id} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return left->id == ((SINCENode*)&other)->left->id && right->id == ((SINCENode*)&other)->right->id &&
-               time.from == ((SINCENode*)&other)->time.from && time.to == ((SINCENode*)&other)->time.to;
+    bool operator==(const SINCENode& other) {
+        return left->id == other.left->id && right->id == other.right->id &&
+               time.from == other.time.from && time.to == other.time.to;
     }
 };
 
@@ -121,9 +172,8 @@ public:
     size_t hash() {
         return ::hash( {op, child->id} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return child->id == ((PRENode*)&other)->child->id;
+    bool operator==(const PRENode& other) {
+        return child->id == other.child->id;
     }
 };
 
@@ -136,9 +186,8 @@ public:
     size_t hash() {
         return ::hash( {op, time.from, time.to, child->id} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return child->id == ((ONCENode*)&other)->child->id && time.from == ((ONCENode*)&other)->time.from && time.to == ((ONCENode*)&other)->time.to;
+    bool operator==(const ONCENode& other) {
+        return child->id == other.child->id && time.from == other.time.from && time.to == other.time.to;
     }
 };
 
@@ -151,9 +200,8 @@ public:
     size_t hash() {
         return ::hash( {op, time.from, time.to, child->id} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return child->id == ((HISTORICALLYNode*)&other)->child->id && time.from == ((HISTORICALLYNode*)&other)->time.from && time.to == ((HISTORICALLYNode*)&other)->time.to;
+    bool operator==(const HISTORICALLYNode& other) {
+        return child->id == other.child->id && time.from == other.time.from && time.to == other.time.to;
     }
 };
 
@@ -166,9 +214,8 @@ public:
     size_t hash() {
         return ::hash( {op, child->id} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return child->id == ((EXISTSNode*)&other)->child->id;
+    bool operator==(const EXISTSNode& other) {
+        return child->id == other.child->id;
     }
 };
 
@@ -181,9 +228,8 @@ public:
     size_t hash() {
         return ::hash( {op, child->id} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return child->id == ((FORALLNode*)&other)->child->id;
+    bool operator==(const FORALLNode& other) {
+        return child->id == other.child->id;
     }
 };
 
@@ -196,9 +242,8 @@ public:
     size_t hash() {
         return ::hash( {op, left->id, right->id} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return left->id == ((IMPLIESNode*)&other)->left->id && right->id == ((IMPLIESNode*)&other)->right->id;
+    bool operator==(const IMPLIESNode& other) {
+        return left->id == other.left->id && right->id == other.right->id;
     }
 };
 
@@ -214,9 +259,8 @@ public:
         int hash_value = hash_fn(value);
         return ::hash( {op, hash_var, hash_value} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return var == ((ATOMNode*)&other)->var && value == ((ATOMNode*)&other)->value;
+    bool operator==(const ATOMNode& other) {
+        return var == other.value;
     }
 };
 
@@ -232,9 +276,8 @@ public:
         int hash_reference = hash_fn(reference);
         return ::hash( {op, hash_var, hash_reference} );
     }
-    bool operator==(const LTLNode& other) {
-        if (op != other.op) return false;
-        return var == ((ATOM_REFNode*)&other)->var && reference == ((ATOM_REFNode*)&other)->reference;
+    bool operator==(const ATOM_REFNode& other) {
+        return var == other.reference;
     }
 };
 
